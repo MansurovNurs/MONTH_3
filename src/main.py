c44df3e87c4e6450e29407c860896eada7bfd50e
Nursultan <mansurov.050895@gmail.com>
2025-04-13 02:00:24 +0300

    Homework 2

--- src/main.py
+++ src/main.py
@@ -3,20 +3,34 @@ import flet as ft
 def main(page: ft.Page):
     page.title = "Hi Guys"
 
-    friends = ["Tima", "Beka", "Adi"]
+    def click_button(e):
 
-    result_text = ft.Text()
-
-    def find_name(e):
-        input_value = name_input.value.strip().title()
-        if input_value in friends:
-            result_text.value = f"✅ Drug: {input_value}"
-        else:
-            result_text.value = "❌ Ne drug."
-        page.update()
-
-    name_input = ft.TextField(label="Enter name", on_change=find_name)
+        age = int(age_input.value)
     
-    page.add(name_input, result_text)
+
+        data_list.append(hobby_input.value)
+        personal_data.append({'name': name_input.value, 'age':age})
+                  
+
+        print(f':\nPersonal data: \n{personal_data}\n Hobbies: {data_list}')
+
+
+
+
+    name_input = ft.TextField(label="Enter name: ")
+    hobby_input = ft.TextField(label='Enter hobby: ')
+    age_input = ft.TextField('only numbers', label='Enter age: ')
+    
+    
+          
+    
+    data_list = []
+    personal_data = []
+
+    button = ft.ElevatedButton("Save it", on_click=click_button)
+    page.add(name_input, age_input, hobby_input, button)
+
 
 ft.app(main)
+
+
